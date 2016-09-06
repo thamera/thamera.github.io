1120f491237292b0efa753869d2b3fc151d47085
thamera <architect@hamera.com>
2016-09-05 20:56:35 -0700

    First Commit

---
+++ app/app.module.js
@@ -0,0 +1,33 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var platform_browser_1 = require('@angular/platform-browser');
+var ng2_bootstrap_1 = require('ng2-bootstrap/ng2-bootstrap');
+var app_component_1 = require('./app.component');
+var home_component_1 = require('./home/home.component');
+var cardprinter_component_1 = require('./cardprinter/cardprinter.component');
+var sprintreport_component_1 = require('./sprintreport/sprintreport.component');
+var app_routing_1 = require('./app.routing');
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        core_1.NgModule({
+            imports: [platform_browser_1.BrowserModule, ng2_bootstrap_1.Ng2BootstrapModule, app_routing_1.routing],
+            declarations: [app_component_1.AppComponent, cardprinter_component_1.cardprinterComponent, sprintreport_component_1.sprintreportComponent, home_component_1.homeComponent],
+            bootstrap: [app_component_1.AppComponent]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppModule);
+    return AppModule;
+}());
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
---
+++ systemjs.config.js
@@ -0,0 +1,53 @@
+﻿/**
+ * System configuration for Angular 2 samples
+ * Adjust as necessary for your application needs.
+ */
+(function (global) {
+    var paths = {
+        // paths serve as alias
+        'npm:': 'node_modules/'
+    }
+    // map tells the System loader where to look for things
+    var map = {
+        'app': 'app', // 'dist',
+        '@angular': 'node_modules/@angular',
+        'moment': 'node_modules/moment/moment.js',
+        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+        'rxjs': 'node_modules/rxjs'
+    };
+    // packages tells the System loader how to load when no filename and/or no extension
+    var packages = {
+        'app': { main: 'main.js', defaultExtension: 'js' },
+        'rxjs': { defaultExtension: 'js' },
+        'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+    };
+    var ngPackageNames = [
+      'common',
+      'compiler',
+      'core',
+      'forms',
+      'http',
+      'platform-browser',
+      'platform-browser-dynamic',
+      'router',
+      'upgrade',
+    ];
+    // Individual files (~300 requests):
+    function packIndex(pkgName) {
+        packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    }
+    // Bundled (~40 requests):
+    function packUmd(pkgName) {
+        packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+    }
+    // Most environments should use UMD; some (Karma) need the individual index files
+    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+    // Add package entries for angular packages
+    ngPackageNames.forEach(setPackageConfig);
+    var config = {
+        paths: paths,
+        map: map,
+        packages: packages
+    };
+    System.config(config);
+})(this);
