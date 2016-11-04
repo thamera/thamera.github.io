ad2b3f9c251973de90fff91e47e59ce353c59326
thamera <architect@hamera.com>
2016-11-03 19:30:23 -0700

    card buttons

---
+++ scripts/cardbutton.js
@@ -0,0 +1,22 @@
+﻿//var Promise = TrelloPowerUp.Promise;
+//var t = TrelloPowerUp.iframe();
+console.dir(this);
+console.dir(window);
+
+(function (t) {
+    console.log("cardbutton...");
+    console.dir(t);
+    t.render(function () {
+        // this function we be called once on initial load
+        // and then called each time something changes that
+        // you might want to react to, such as new data being
+        // stored with t.set()
+        console.log("TrelloPowerUp:");
+        console.dir(t);
+        t.card('name')
+            .getAll()
+            .then(function (data) {
+                console.dir(data);
+            });
+    });
+})(TrelloPowerUp);
--- scripts/powerup.js
+++ scripts/powerup.js
@@ -5,6 +5,36 @@
     });
 };
 
+var cardButtonCallback = function (t, options) {
+    return t.popup({
+        title: 'Test',
+        url: './cardbutton.html',
+        width: 400,
+        args: {test2: "test2"}
+    })
+}
+
+var getBadges = function (t) {
+    console.log("powerup > getBadges");
+
+    return t.card('name')
+        .get('name')
+        .then(function (cardName) {
+            var badgeColor;
+            var icon = './images/pci38.png';
+
+            var lowercaseName = cardName.toLowerCase();
+            badgeColor = 'red';
+
+            return [{
+                title: 'Testing Badge', //detail badges only
+                text: 'Test Text',
+                icon: icon,
+                color: badgeColor
+            }];
+        });
+}
+
 TrelloPowerUp.initialize({
     'board-buttons': function (t, board) {
         return [
@@ -15,16 +45,27 @@ TrelloPowerUp.initialize({
             }
         ];
     },
-    'show-settings': function (t, options) {
-        return t.overlay({
-            url: './#/settings'
-        });
-        //return t.popup({
-        //    title: 'Settings',
-        //    url: './settings.html',
-        //    width: 300
-        //});
-
-
+    'card-buttons': function (t, options) {
+        return [{
+            icon: './images/pci38.png',
+            text: 'Card Fields',
+            callback: cardButtonCallback
+        }]
+    },
+    'card-badges': function (t, options) {
+        return getBadges(t);
+    },
+    'card-detail-badges': function (t, options) {
+        return getBadges(t);
     }
+
+
+    //,
+    //'show-settings': function (t, options) {
+    //    //return t.popup({
+    //    //    title: 'Settings',
+    //    //    url: './settings.html',
+    //    //    width: 300
+    //    //});
+    //}
 });
